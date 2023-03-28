6ec709d69165911fe3fca01e3567a7fb59fd0d4f
Roei Erez <roeierez@gmail.com>
2023-03-28 11:07:04 +0300

    start sdk

--- lib/sdk_bridge.dart
+++ lib/sdk_bridge.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:breez_sdk/breez_bridge.dart';
 import 'package:breez_sdk/bridge_generated.dart' as sdk;
 import 'package:breez_sdk/bridge_generated.dart';
@@ -9,13 +11,39 @@ import 'package:path_provider/path_provider.dart';
 import 'conf.dart';
 
 extension InitSDK on BreezBridge {
+  
+  Future start() async {
+    const secureStorage = FlutterSecureStorage();
+    var glCert = await secureStorage.read(key: "gl-cert");
+    var glKey = await secureStorage.read(key: "gl-key");
+    var mnemonics = await secureStorage.read(key: "mnemonics");
+    if (glCert != null && glKey != null && mnemonics != null) {
+      await _initExistingdNode(mnemonics, glCert, glKey);
+    } else {
+      await _initNewNode();
+    }
+    await startNode();
+  }
+
+  Future _initExistingdNode(String mnemonic, String glCert, String glKey) async {
+    final seed = bip39.mnemonicToSeed(mnemonic);
+    final sdkConfig = await getConfig();
+    await initServices(
+      config: sdkConfig,
+      seed: seed,
+      creds: sdk.GreenlightCredentials(
+        deviceCert: Uint8List.fromList(HEX.decode(glCert)),
+        deviceKey: Uint8List.fromList(HEX.decode(glKey)),
+      ),
+    );
+  }
 
   Future _initNewNode() async {
-    const secureStorage = FlutterSecureStorage();    
+    const secureStorage = FlutterSecureStorage();
     final mnemonics = bip39.generateMnemonic();
     final seed = bip39.mnemonicToSeed(mnemonics);
     final sdkConfig = await getConfig();
-    
+
     final sdk.GreenlightCredentials creds = await registerNode(
       config: sdkConfig,
       network: sdk.Network.Bitcoin,
@@ -27,8 +55,7 @@ extension InitSDK on BreezBridge {
   }
 
   Future<Config> getConfig() async {
-    return (await defaultConfig(sdk.EnvironmentType.Production)).copyWith(
-        workingDir: (await getApplicationDocumentsDirectory()).path,
-        apiKey: apiKey);
+    return (await defaultConfig(sdk.EnvironmentType.Production))
+        .copyWith(workingDir: (await getApplicationDocumentsDirectory()).path, apiKey: apiKey);
   }
 }
